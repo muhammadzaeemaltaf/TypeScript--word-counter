7350800d562c02faa0e11fb6d83fed92b8349e59
muhammadzaeemaltaf <165184980+muhammadzaeemaltaf@users.noreply.github.com>
2024-04-18 17:27:09 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,15 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+const answer = await inquirer.prompt([
+{
+    name : "sentence",
+    type : "input",
+    message: "Enter your sentence to count the words: ",
+},
+]);
+
+const words = answer.sentence.trim().split(" ");
+
+console.log(`Your sentence word count is ${words.length}`);
